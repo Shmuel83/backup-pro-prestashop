52c242b2384ec4739faff36ddc2cf86cb7799585
Eric Lamb <eric@mithra62.com>
2016-01-27 20:46:20 -0800

    Fixes path issue with Console

--- modules/backup_pro/console.php
+++ modules/backup_pro/console.php
@@ -7,21 +7,25 @@
  * @version		3.0
  * @filesource 	./console.php
  */
-if (php_sapi_name() !== 'cli') {
-    exit();
+
+if( php_sapi_name() !== 'cli' ){
+    exit;
 }
 
-if (! file_exists('./cli.config.php')) {
+$old_wd = getcwd();
+chdir(__DIR__);
+
+if( !file_exists( './cli.config.php') ){
     echo "Config file not set...";
-    exit();
+    exit;
 }
 
-// error_reporting(E_ALL | E_STRICT);
-// ini_set('display_errors', 1);
+//error_reporting(E_ALL | E_STRICT);
+//ini_set('display_errors', 1);
 ini_set('log_errors', 0);
 ini_set('html_errors', 0);
 
-require_once dirname(__FILE__) . '/includes/vendor/autoload.php';
+require_once dirname(__FILE__).'/vendor/autoload.php';
 
 use mithra62\BackupPro\Platforms\Controllers\Console;
 
@@ -29,3 +33,4 @@ $config = include './cli.config.php';
 
 $controller = new Console($config);
 $controller->run();
+chdir($old_wd);
