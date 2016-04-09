8a50131c6e6b3faeaed3e1685702fad85e4ab4df
Eric Lamb <eric@mithra62.com>
2016-04-09 10:56:14 -0700

    Adds check for requirements before installation can happen

--- modules/backup_pro/backup_pro.php
+++ modules/backup_pro/backup_pro.php
@@ -79,6 +79,22 @@ class Backup_pro extends Module implements \mithra62\BackupPro\BackupPro
      */
     public function install()
     {
+        if (version_compare(phpversion(), '5.4.0', '<')) {
+            throw new \Exception('You must be run PHP 5.4 or greater to use this package.');
+        }
+         
+        if (!function_exists('mysqli_report') && !class_exists('PDO')) {
+            throw new \Exception('Backup Pro requires either the mysqli or PDO extension for database use.');
+        }
+         
+        if (!function_exists('mb_check_encoding')) {
+            throw new \Exception('Backup Pro requires the Multi Byte extension to ensure proper string encoding.');
+        }
+         
+        if (!function_exists('curl_init')) {
+            throw new \Exception('Backup Pro requires the Curl extension to transfer backups to remote locations.');
+        }
+        
         if (! parent::install() || ! $this->installSettingsTable() || ! $this->registerHook('displayBackOfficeHeader') || ! $this->installModuleTabs()) {
             return false;
         }
